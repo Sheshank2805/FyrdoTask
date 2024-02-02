449180adf923df594abbc35e567589d261158005
Sheshank2805 <danthanapallysheshank123@gmail.com>
2024-02-02 13:45:31 +0530

    updated banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,34 +4,38 @@ import bannerImg from "../assets/bannerimg.jpg";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
-  const bannerStyle = {
+  const containerStyle = {
     backgroundImage: `url(${bannerImg})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
+    width: "100%",
   };
 
   return (
-    <div className="relative top-0 overflow-hidden" style={bannerStyle}>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="relative w-full">
+      <div className="absolute inset-0" style={containerStyle}></div>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="flex flex-col md:flex-row items-center justify-between py-16 md:py-32 lg:py-40">
-          <div className="md:w-1/2 lg:w-2/3">
-            <p className="text-white text-4xl md:text-5xl lg:text-6xl font-bold leading-normal">
+          <div className="md:w-full lg:w-2/3 mb-8 md:mb-0">
+            <p className="text-white mr-4 text-4xl md:text-5xl lg:text-6xl font-bold leading-normal">
               Effortlessly go live with your event in a go!
             </p>
-            <p className="text-white text-base md:text-lg lg:text-xl mt-4 leading-6">
-              Empower your events, whether on-ground or digital, with our user-friendly platform loved by millions, making event creation and hosting a breeze.
+            <p className="text-white mr-4 text-base md:text-lg lg:text-xl mt-4 leading-6">
+              Empower your events, whether on-ground or digital, with our
+              user-friendly platform loved by millions, making event creation
+              and hosting a breeze.
             </p>
-            <div className="mt-6">
+            <div className="mt-10 md:mt-0 md:mr-[520px]">
               <Link to="form">
-                <button className="bg-[#fe3838] text-white py-2 px-6 rounded-full text-sm md:text-base lg:text-lg font-bold">
+                <button className="bg-[#fe3838] text-white mr-4 py-2 px-6 rounded-full text-sm md:text-base lg:text-lg font-bold">
                   LIST YOUR EVENT
                 </button>
               </Link>
             </div>
           </div>
-          <div className="md:w-1/2 lg:w-1/3 mt-8 md:mt-0">
+          <div className="md:w-full lg:w-1/3 md:absolute md:top-0 md:right-0 md:bottom-0 md:left-auto">
             <img
-              className="w-full h-auto md:max-w-lg lg:max-w-xl mx-auto"
+              className="w-full h-full mb-0 md:max-w-lg lg:max-w-xl mx-auto rounded-md"
               alt="Iphone white"
               src={backgroundImage}
             />
--- src/Components/Metrics.jsx
+++ src/Components/Metrics.jsx
@@ -3,7 +3,7 @@ import MetricItem from "./MetricItem";
 
 const Metrics = () => {
   return (
-    <div className="flex flex-col w-full sm:w-full md:w-[720px] lg:w-[960px] xl:w-[1440px] items-center gap-4 sm:gap-6 md:gap-8 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-8 sm:py-10 md:py-12 lg:py-16 xl:py-20 relative bg-[#0b0d17]">
+    <div className="flex flex-col items-center gap-4 sm:gap-6 md:gap-8 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-8 sm:py-10 md:py-12 lg:py-16 xl:py-20 relative bg-[#0b0d17]">
       <div className="flex flex-col items-center gap-4 sm:gap-6 md:gap-8 relative self-stretch w-full">
         <p className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-center">
           Our Metrics Tell the Story
@@ -12,7 +12,7 @@ const Metrics = () => {
           Explore Compelling Insights Uncovered by Our Metrics and Data Analysis
         </p>
       </div>
-      <div className="flex flex-col sm:flex-row w-full sm:w-full md:w-[720px] lg:w-[960px] xl:w-[1170px] items-start gap-4 sm:gap-6 md:gap-8 relative">
+      <div className="flex flex-col sm:flex-row w-full items-start gap-4 sm:gap-6 md:gap-8 relative mx-auto">
         <MetricItem
           className="border-[#fe3838] flex-1 border-b-4 shadow-md grow w-full sm:w-full md:w-1/3"
           label="Happy Organisers"
