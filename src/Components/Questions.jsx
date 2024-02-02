472e1faad177ec57cd07e4b3305f694b5587294f
Sheshank2805 <danthanapallysheshank123@gmail.com>
2024-02-02 14:09:09 +0530

    updating Questions

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,54 +1,58 @@
 import React from 'react';
-import frame1img from "../assets/Frame (1).jpg"
-import frameImg from "../assets/Frame.jpg"
 
 const Questions = () => {
   return (
-    <div className="w-full md:w-[1024px] mx-auto px-4 md:px-[120px] py-6 rounded-lg flex-col justify-start items-center gap-6 inline-flex">
+<div id="accordion-color" data-accordion="collapse" data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white">
+<div><h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-4">Frequently Asked Questions</h1></div>
+  <h2 id="accordion-color-heading-1">
+    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-color-body-1" aria-expanded="true" aria-controls="accordion-color-body-1">
+      <span>What is Greet?</span>
+      <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
+  </h2>
+  <div id="accordion-color-body-1"  aria-labelledby="accordion-color-heading-1">
+    <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+      <p className="mb-2 text-gray-500 dark:text-gray-400"> Greet is a subsidiary of Fydo (Lfyd Services Pvt Ltd), an event booking platform designed to streamline the process of organizing and attending events. It offers a user-friendly interface for event organizers and attendees.</p>
+  </div>
+  <h2 id="accordion-color-heading-2">
+    <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-color-body-2" aria-expanded="false" aria-controls="accordion-color-body-2">
+      <span>What’s Unique about Greet?</span>
+      <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
+  </h2>
+  </div>
+  <h2 id="accordion-color-heading-3">
+    <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-color-body-3" aria-expanded="false" aria-controls="accordion-color-body-3">
+      <span>How can I book tickets for an event on Greet?</span>
+      <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
+  </h2>
+  <h2 id="accordion-color-heading-3">
+    <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-color-body-3" aria-expanded="false" aria-controls="accordion-color-body-3">
+      <span>Can I use cashback earned at partner shops to book event tickets?</span>
+      <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
+  </h2>
+  <h2 id="accordion-color-heading-3">
+    <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-color-body-3" aria-expanded="false" aria-controls="accordion-color-body-3">
+      <span>Can I sell tickets for my own event on Greet?</span>
+      <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
+  </h2>
+  
+</div>
 
-      
-      <div><h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-4">Frequently Asked Questions</h1></div>
 
-      
-      <div className="w-full flex-col justify-start items-start gap-4 flex">
-        <div className="w-full border-b border-gray-200 pb-4">
-          <div className="flex justify-between items-center">
-            <div className="text-zinc-800 text-xl md:text-2xl lg:text-3xl font-semibold leading-7">What is Greet?<span><img src={frameImg} alt="frame"/></span></div>
-            <div className="w-8 h-8 relative origin-top-left -rotate-180" />
-          </div>
-          <div className="pr-10 justify-start items-start gap-2">
-            <div className="text-slate-500 text-base font-normal leading-normal">
-              Greet is a subsidiary of Fydo (Lfyd Services Pvt Ltd), an event booking platform designed to streamline the process of organizing and attending events. It offers a user-friendly interface for event organizers and attendees.
-            </div>
-          </div>
-        </div>
-        <div className="w-full border-b border-gray-200 pb-4">
-          <div className="flex justify-between items-center">
-            <div className="text-zinc-800 text-xl md:text-2xl lg:text-3xl font-semibold leading-7">What’s Unique about Greet?<span><img src={frame1img} alt="frame1"/></span></div>
-            <div className="w-8 h-8 relative" />
-          </div>
-        </div>
-        <div className="w-full border-b border-gray-200 pb-4">
-          <div className="flex justify-between items-center">
-            <div className="text-zinc-800 text-xl md:text-2xl lg:text-3xl font-semibold leading-7">How can I book tickets for an event on Greet?<span><img src={frame1img} alt="frame1"/></span></div>
-            <div className="w-8 h-8 relative" />
-          </div>
-        </div>
-        <div className="w-full border-b border-gray-200 pb-4">
-          <div className="flex justify-between items-center">
-            <div className="text-zinc-800 text-xl md:text-2xl lg:text-3xl font-semibold leading-7">Can I use cashback earned at partner shops to book event tickets?<span><img src={frame1img} alt="frame1"/></span></div>
-            <div className="w-8 h-8 relative" />
-          </div>
-        </div>
-        <div className="w-full pb-4">
-          <div className="flex justify-between items-center">
-            <div className="text-zinc-800 text-xl md:text-2xl lg:text-3xl font-semibold leading-7">Can I sell tickets for my own event on Greet?<span><img src={frame1img} alt="frame1"/></span></div>
-            <div className="w-8 h-8 relative" />
-          </div>
-        </div>
-
-      </div>
-    </div>
   );
 };
 
